b4ed642fa39a24cf59051d446e115bc20f6ec48e
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-09-24 20:52:14 +0100

    Trending page, modal movie page

--- src/components/Trends/Trends.tsx
+++ src/components/Trends/Trends.tsx
@@ -1,30 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { Grid, Typography } from "@mui/material";
+import React, { useEffect, useState, useCallback } from "react";
+import { Grid, Typography, Badge } from "@mui/material";
 import axios from "axios";
 import Pagination from "@mui/material/Pagination";
+import genreDictionary from "../Genres/Genre";
+import { Movie } from "../../types/movie";
+import MovieModal from "../MovieModal/MovieModal"; 
 
 const Trends: React.FC = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 4;
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${currentPage}`
-        );
+  const [totalPages, setTotalPages] = useState(1);
+ 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-        setMovies(response.data.results);
-      } catch (error) {
-        console.error("Error fetching data:", error);
+  const handleMovieClick = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
+  const closeMovieModal = () => {
+    setSelectedMovie(null);
+    setIsModalOpen(false);
+  };
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${currentPage}`
+      );
+
+      setMovies(response.data.results);
+      setTotalPages(response.data.total_pages);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }, [currentPage]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    // Обработчик кликов вне модального окна
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        selectedMovie &&
+        isModalOpen &&
+        event.target instanceof Node &&
+        !document.getElementById("movie-modal")?.contains(event.target)
+      ) {
+        closeMovieModal();
       }
     };
 
-    fetchData();
-  }, [currentPage]);
+    if (isModalOpen) {
+      // Добавляем обработчик для кликов вне модального окна при открытии модального окна
+      window.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      // Удаляем обработчик при закрытии модального окна
+      window.removeEventListener("mousedown", handleOutsideClick);
+    }
 
-  const handleChangePage = (event: React.ChangeEvent<unknown>, newPage: number) => {
+    return () => {
+      // Удаляем обработчик при размонтировании компонента
+      window.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isModalOpen, selectedMovie]);
+
+  const handleChangePage = (
+    event: React.ChangeEvent<unknown>,
+    newPage: number
+  ) => {
     setCurrentPage(newPage);
     window.scrollTo(0, 0);
   };
@@ -32,22 +79,35 @@ const Trends: React.FC = () => {
   return (
     <>
       <Grid container spacing={4} className="movie-container">
-        {movies.map((movie: any) => (
+        {movies.map((movie) => (
           <Grid item key={movie.id} xs={12} sm={6} md={3}>
-            <div className="movie-card">
+            <Badge
+              badgeContent={movie.vote_average.toFixed(1)}
+              color={movie.vote_average > 7 ? "success" : "error"}
+            ></Badge>
+            <div
+              className="movie-card"
+              onClick={() => handleMovieClick(movie)} 
+            >
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
                 className="movie-poster"
               />
-              <Typography variant="h6">{movie.title}</Typography>
-              <Typography variant="body2">{movie.release_date}</Typography>
-              <Typography variant="body2">{movie.runtime} minutes</Typography>
+              <Typography variant="h6">
+                {movie.title || movie.name || "N/A"}
+              </Typography>
+              <Typography variant="body2">
+                {movie.release_date || movie.first_air_date || "N/A"}
+              </Typography>
               <Typography variant="body2">
                 Genres:{" "}
                 {movie.genre_ids
                   ? movie.genre_ids
-                      .map((genre: any) => movie.genre_ids)
+                      .map((genreId: number) => {
+                        const genreName = genreDictionary[genreId];
+                        return genreName || "N/A";
+                      })
                       .join(", ")
                   : "N/A"}
               </Typography>
@@ -55,13 +115,21 @@ const Trends: React.FC = () => {
           </Grid>
         ))}
       </Grid>
-      {movies.length > itemsPerPage && (
+      {totalPages > 1 && (
         <Pagination
-          count={Math.ceil(movies.length / itemsPerPage)}
+          count={totalPages}
           page={currentPage}
           onChange={handleChangePage}
           variant="outlined"
           shape="rounded"
+        />
+      )}
+
+      {isModalOpen && selectedMovie && (
+        <MovieModal
+          isOpen={isModalOpen}
+          onClose={closeMovieModal}
+          movie={selectedMovie}
         />
       )}
     </>
